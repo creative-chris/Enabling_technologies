9751ba0596ee935e27a307f0ccc150e4cd896670
creative-chris <xcreatief@gmail.com>
2024-06-20 22:00:37 +0200

    Finshed setup

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0" />
+    <link href="style.css" type="text/css" rel="stylesheet" media="screen"/>
+    <link rel="icon" href="Logo/makam-logo_black-icon.svg">
+    <title>Enabling Technologies</title>
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/animation.js" defer></script>
+    <script src="js/error.js" defer></script>
+</head>
+<body>
+    <div id="container">
+        <header></header>
+        <main></main>
+        <footer></footer>
+    </div>
+</body>
+</html>
