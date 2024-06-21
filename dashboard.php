da08410ce1b80eef128542ce87b3997a6effa636
creative-chris <xcreatief@gmail.com>
2024-06-21 13:00:02 +0200

    Setup dashboard page and styling

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,15 @@
 <div id="dashboard" class="page">
-    
+    <div id="dashboard-header">
+        <img src="image/logo.png" />
+    </div>
+    <div id="inner-dashboard">
+        <ul id="dashboard-list" class="list">
+            <a><li class="dashboard-list-item list-item"></li></a>
+            <a><li class="dashboard-list-item list-item"></li></a>
+            <a><li class="dashboard-list-item list-item"></li></a>
+            <a><li class="dashboard-list-item list-item"></li></a>
+            <a><li class="dashboard-list-item list-item"></li></a>
+            <a><li class="dashboard-list-item list-item"></li></a>
+        </ul>
+    </div>
 </div>
