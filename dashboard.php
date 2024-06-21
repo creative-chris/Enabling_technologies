47a6173555f4170fe7780ea275c070b7e3d5a18d
creative-chris <xcreatief@gmail.com>
2024-06-21 14:45:22 +0200

    dashboard setup innovations

--- dashboard.php
+++ dashboard.php
@@ -4,12 +4,12 @@
     </div>
     <div id="inner-dashboard">
         <ul id="dashboard-list" class="list">
-            <a><li class="dashboard-list-item list-item"></li></a>
-            <a><li class="dashboard-list-item list-item"></li></a>
-            <a><li class="dashboard-list-item list-item"></li></a>
-            <a><li class="dashboard-list-item list-item"></li></a>
-            <a><li class="dashboard-list-item list-item"></li></a>
-            <a><li class="dashboard-list-item list-item"></li></a>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-1"><div class="inner-list-item"></div></li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-2"><div class="inner-list-item"></div></li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-3"><div class="inner-list-item"></div></li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-4"><div class="inner-list-item"></di></li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-5"><div class="inner-list-item"></div></li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-6"><div class="inner-list-item"></div></li>
         </ul>
     </div>
 </div>
