5472b4c0f6fb2958ce7a461f685e771d47d5122f
creative-chris <xcreatief@gmail.com>
2024-06-21 07:00:34 +0200

    Creation- components and their css

---
+++ head.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0" />
+    
+    <!-- CSS section -->
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen"/>
+    <link href="css/responsive.css" type="text/css" rel="stylesheet" media="screen"/>
+    <link href="intro.css" type="text/css" rel="stylesheet" media="screen"/>
+    <link href="dashboard.css" type="text/css" rel="stylesheet" media="screen"/>
+    <link href="innovation.css" type="text/css" rel="stylesheet" media="screen"/>
+
+    <!-- Site title & icon -->
+    <title>Enabling Technologies</title>
+    <link rel="icon" href="Logo/makam-logo_black-icon.svg">
+
+    <!-- Javascript section -->
+    <script src="js/jquery-3.7.1.min.js"></script> 
+    <script src="js/animation.js" defer></script>
+    <script src="js/error.js" defer></script>
+</head>
---
+++ innovation.php
@@ -0,0 +1,3 @@
+<div id="AI" class="innovation">
+    
+</div>
