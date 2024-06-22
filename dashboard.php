0febc4850ed6115b2cd5d14baf7d808551df0035
Christian Kit <57896689+creative-chris@users.noreply.github.com>
2024-06-22 11:10:21 +0200

    enabling tech icons and centering

--- dashboard.php
+++ dashboard.php
@@ -3,13 +3,45 @@
         <img src="image/logo.png" />
     </div>
     <div id="inner-dashboard">
+        <!-- List with enabling technologies -->
         <ul id="dashboard-list" class="list">
-            <li class="dashboard-list-item list-item" id="dashboard-list-item-1"><div class="inner-list-item"></div></li>
-            <li class="dashboard-list-item list-item" id="dashboard-list-item-2"><div class="inner-list-item"></div></li>
-            <li class="dashboard-list-item list-item" id="dashboard-list-item-3"><div class="inner-list-item"></div></li>
-            <li class="dashboard-list-item list-item" id="dashboard-list-item-4"><div class="inner-list-item"></di></li>
-            <li class="dashboard-list-item list-item" id="dashboard-list-item-5"><div class="inner-list-item"></div></li>
-            <li class="dashboard-list-item list-item" id="dashboard-list-item-6"><div class="inner-list-item"></div></li>
+            <!-- List items -->
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-1">
+                <div class="inner-list-item">
+                    <img src="image/icons/quantum.png" class="list-icon" /><br>
+                    <label class="list-icon-label">Quantum Sensors & Photonics</label>
+                </div>
+            </li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-2">
+                <div class="inner-list-item">
+                    <img src="image/icons/ai.png" class="list-icon" /><br>
+                    <label class="list-icon-label">Artificial Intelligence</label>
+                </div>
+            </li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-3">
+                <div class="inner-list-item">
+                    <img src="image/icons/autonomous.png" class="list-icon" /><br>
+                    <label class="list-icon-label">Autonomous Robots</label>
+                </div>
+            </li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-4">
+                <div class="inner-list-item">
+                    <img src="image/icons/energy.png" class="list-icon" /><br>
+                    <label class="list-icon-label">Energy Storage</label>
+                </di>
+            </li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-5">
+                <div class="inner-list-item">
+                    <img src="image/icons/application.png" class="list-icon" /><br>
+                    <label class="list-icon-label">Application Materials</label>
+                </div>
+            </li>
+            <li class="dashboard-list-item list-item" id="dashboard-list-item-6">
+                <div class="inner-list-item">
+                    <img src="image/icons/digital.png" class="list-icon" /><br>
+                    <label class="list-icon-label">Digital Twinning & IoT</label>
+                </div>
+            </li>
         </ul>
     </div>
 </div>
