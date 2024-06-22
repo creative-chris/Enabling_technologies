3ec46c315a13b36e2274e9023d9e6829c1243331
Christian Kit <57896689+creative-chris@users.noreply.github.com>
2024-06-22 14:16:40 +0200

    finished setup and styling innovation page (ai)

--- innovation.php
+++ innovation.php
@@ -1,3 +1,46 @@
 <div id="AI" class="innovation">
-    
+    <div class="innovation-header">
+        <nav class="innovation-header-nav">
+            <ul>
+                <li>
+                    <img src="image/icons/quantum.png" id="icon1"/>
+                    <img src="image/icons/ai.png" class="current" id="icon2"/>
+                    <img src="image/icons/autonomous.png" id="icon3"/>
+                    <img src="image/icons/energy.png" id="icon4"/>
+                    <img src="image/icons/application.png" id="icon5"/>
+                    <img src="image/icons/digital.png" id="icon6"/>
+                </li>
+                <li><img src="image/logo.png" /></li>
+            </ul>
+        </nav>
+    </div>
+    <img src="image/background/9e74ecd8e520cf095619105fac7ed34a.jpg" class="innovation-background"/>
+    <div class="inner-innovation">
+        <h3>What is artificial intelligence (AI)?</h3><br>
+        <br>
+        <p>Artificial intelligence is the simulation of human intelligence processes by machines, especially computer systems. Specific applications of AI include expert systems, natural language processing, speech recognition and machine vision.
+        </p>
+        <br>
+        <b>Examples of AI</b><br>
+        <br>
+        <a href="https://viggle.ai/" target="_blank">viggle.ai</a> | AI 3d video. <br>
+        <a href="https://www.immersity.ai/" target="_blank">immersity.ai</a> | Turning 2D images into 3D images.<br>
+        <a href="https://ideogram.ai/t/explore" target="_blank">ideogram.ai</a> | image generator<br>
+        <br>
+        <b>How does AI work?</b>
+        <br>
+        <br>
+        <p>As the hype around AI has accelerated, vendors have been scrambling to promote how their products and services use it. Often, what they refer to as AI is simply a component of the technology, such as machine learning. AI requires a foundation of specialized hardware and software for writing and training machine learning algorithms. No single programming language is synonymous with AI, but Python, R, Java, C++ and Julia have features popular with AI developers.
+        </p><br>
+        <p>In general, AI systems work by ingesting large amounts of labeled training data, analyzing the data for correlations and patterns, and using these patterns to make predictions about future states. In this way, a chatbot that is fed examples of text can learn to generate lifelike exchanges with people, or an image recognition tool can learn to identify and describe objects in images by reviewing millions of examples. New, rapidly improving generative AI techniques can create realistic text, images, music and other media.</p>
+        <br>
+        <b>Why is artificial intelligence important?</b><br>
+        <br>
+        <p>AI is important for its potential to change how we live, work and play. It has been effectively used in business to automate tasks done by humans, including customer service work, lead generation, fraud detection and quality control. In a number of areas, AI can perform tasks much better than humans. Particularly when it comes to repetitive, detail-oriented tasks, such as analyzing large numbers of legal documents to ensure relevant fields are filled in properly, AI tools often complete jobs quickly and with relatively few errors. Because of the massive data sets it can process, AI can also give enterprises insights into their operations they might not have been aware of. The rapidly expanding population of generative AI tools will be important in fields ranging from education and marketing to product design.</p>
+        <br>
+        <br>
+        <form>
+            
+        </form>
+    </div>
 </div>
